e02c9fcbddb8c7e0f40fa23969fa363b28013022
Gabriel <gabriel.mirandab92@gmail.com>
2022-02-26 17:29:33 -0300

    adding dependency injection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,36 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/gabrielmirandaBR/authenticate_go/adapters"
+	"github.com/gabrielmirandaBR/authenticate_go/internal/core/rest"
+	"github.com/gabrielmirandaBR/authenticate_go/internal/core/services"
+	"github.com/gabrielmirandaBR/authenticate_go/internal/repository"
+)
+
 func main() {
+	env, err := adapters.GetEnvironments()
+	if err != nil {
+		fmt.Printf("Error getting environments: %s", err)
+		return
+	}
+
+	db := adapters.NewDatabase(&adapters.Config{
+		Host: env.Host,
+		Username: env.Username,
+		Password: env.Password,
+		DBName: env.DBName,
+		Port: env.Port,
+	})
+	
+	bookRepo := repository.MustNewBook(db)
+	bookService := services.MustNewBook(bookRepo)
+	bookRest := rest.MusNewBook(bookService)
+
 	server := MustNewServer()
 	router := server.Listen()
+	// router.POST("/books", bookRest.)
 
 	router.Run()
 }
