28ef6c9735106a880c1219d18a25a4130916ab20
Gabriel <gabriel.mirandab92@gmail.com>
2022-02-26 14:27:29 -0300

    adding migrates

--- cmd/migrations/main.go
+++ cmd/migrations/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/gabrielmirandaBR/authenticate_go/adapters"
+	"github.com/gabrielmirandaBR/authenticate_go/driven"
+)
+
+
+func main() {
+	env, err := adapters.GetEnvironments()
+	if err != nil {
+		fmt.Printf("Error getting environments: %s", err)
+		return
+	}
+
+	db := adapters.NewDatabase(&adapters.Config{
+		Host: env.Host,
+		Username: env.Username,
+		Password: env.Password,
+		DBName: env.DBName,
+		Port: env.Port,
+	})
+
+
+	tableBook := &driven.Book{}
+
+	err = db.AutoMigrate(tableBook)
+	if err != nil {
+		panic(err)
+	}
+}
