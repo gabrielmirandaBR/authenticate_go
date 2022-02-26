cf2fa4b868fd31f80f5fa82defcbc971f88bce55
Gabriel <gabriel.mirandab92@gmail.com>
2022-02-26 14:26:09 -0300

    adding server

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -1,1 +1,13 @@
 package main
+
+import "github.com/gin-gonic/gin"
+
+type Server struct {}
+
+func MustNewServer() Server {
+	return Server{}
+}
+
+func (s Server) Listen() *gin.Engine {
+	return gin.Default()
+}
