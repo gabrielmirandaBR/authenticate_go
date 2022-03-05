57c3de4ee83f5f38e63e7d474db77561b2ed9f9f
Gabriel <gabriel.mirandab92@gmail.com>
2022-03-05 19:57:03 -0300

    adding logic to createBook repo method

--- driven/dto.go
+++ driven/dto.go
@@ -7,13 +7,13 @@ import (
 )
 
 type Book struct {
-	ID          uint           `json:"id" gorm:"primaryKey"`
-	Name        string         `json:"name"`
-	Description string         `json:"description"`
-	MediumPrice float32        `json:"medium_price"`
-	Author      string         `json:"author"`
-	ImageURL    string         `json:"img_url"`
-	CreatedAt   time.Time      `json:"created"`
-	UpdatedAt   time.Time      `json:"updated"`
-	DeletedAt   gorm.DeletedAt `gorm:"index" json:"deleted"`
+	ID          string         `gorm:"primaryKey"`
+	Name        string         
+	Description string         
+	MediumPrice float32        
+	Author      string         
+	ImageURL    string         
+	CreatedAt   time.Time      
+	UpdatedAt   time.Time      
+	DeletedAt   gorm.DeletedAt `gorm:"index"`
 }
--- internal/repository/book.go
+++ internal/repository/book.go
@@ -2,7 +2,9 @@ package repository
 
 import (
 	"context"
+	"log"
 
+	"github.com/gabrielmirandaBR/authenticate_go/driven"
 	"github.com/gabrielmirandaBR/authenticate_go/internal/core/domain"
 	"gorm.io/gorm"
 )
@@ -23,6 +25,20 @@ func MustNewBook(gormCli *gorm.DB) *Book {
 }
 
 func (b Book) CreateBook(ctx context.Context, book domain.Book)  error {
+	newBook := &driven.Book{
+		ID: book.ID,
+		Name: book.Name,
+		Description: book.Description,
+		MediumPrice: book.MediumPrice,
+		Author: book.Author,
+		ImageURL: book.ImageURL,
+	}
+
+	err := b.cli.Create(&newBook).Error
+	if err != nil {
+		log.Fatalf("%s", err)
+		return err
+	}
 	
 	return  nil
 }
